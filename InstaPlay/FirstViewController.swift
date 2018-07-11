fa37fe30cbd5d770f5fd5da40cb548373d981d6e
Vincent DOUANT <vincent@edupad.com>
2018-07-11 20:16:47 +0200

    Initial Commit

---
+++ InstaPlay/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  InstaPlay
+//
+//  Created by Vincent Douant on 11/07/2018.
+//  Copyright © 2018 Vincent Douant. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
